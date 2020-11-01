b536a2babfc4975eb4fbe01e0a04ec53d3fd7f9b
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-11-01 15:49:10 +0100

    Restructured files and added variable urls for blog posts

---
+++ flaskblog/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.models import Post
+
+@app.route("/")
+@app.route("/starthere")
+def home():
+    return render_template('home.html', title = 'Jvw')
+
+@app.route("/blog")
+def blog():
+    post = Post.query.all()
+    return render_template('blog.html', title = 'Blog ✍️', posts=post)
+
+@app.route("/blog/<int:post_id>/<string:post_url_title>")
+def blog_post(post_url_title, post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('blog_post.html', title=post.title, post=post)
