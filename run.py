3917a52e4f7b801e889729e6a3dab269062a1186
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-11-03 20:43:50 +0100

    added media queries to blog posts to make them more mobile friendly

--- run.py
+++ run.py
@@ -2,7 +2,3 @@ from flaskblog import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-"Learning C++ can open up careers in a wide array of fields ranging from robotics to game programming, but at a cost of about $1000, is Udacity's nanodegree the best way to learn it?"
-"Reviewing Udacity's C++ Nanodegree"
-"ReviewingUdacity'sC++Nanodegree"
