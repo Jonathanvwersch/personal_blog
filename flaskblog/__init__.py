92d2183673a0e5509a553f9ef19c3480969cf930
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-11-11 21:55:42 +0100

    added flask admin

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = "secret_key_test"
+admin = Admin(app)
 
 from flaskblog import routes
