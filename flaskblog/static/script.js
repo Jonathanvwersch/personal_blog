ffb8261d9ecaf99c7b1bb7bc94d499b61f25dde8
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-11-17 22:35:54 +0100

    added dark mode toggle switch

--- flaskblog/static/script.js
+++ flaskblog/static/script.js
@@ -19,14 +19,7 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
-        if (document.getElementById('logo').src) {
-            document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
-        } 
-
-        if ( document.getElementById('profile').srcsrc) {
-            document.getElementById('profile').src  = '/static/profile_black.png';
-        } 
-        
+        document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
         document.querySelector("link[rel='shortcut icon']").href = "/static/favicon_black.svg";
         toggleSwitch.checked = true;
     }
@@ -35,26 +28,12 @@ if (currentTheme) {
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        if (document.getElementById('logo').src) {
-            document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
-        } 
-
-        if ( document.getElementById('profile').srcsrc) {
-            document.getElementById('profile').src  = '/static/profile_black.png';
-        } 
-        
+        document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
         localStorage.setItem('theme', 'dark');
     }
     else {        
         document.documentElement.setAttribute('data-theme', 'light');
-        if (document.getElementById('logo').src) {
-            document.getElementById('logo').src = '/static/logo_jvw.svg';
-        }  
-
-        if (document.getElementById('profile').src) {
-            document.getElementById('profile').src  = '/static/profile.png';
-        } 
-        
+        document.getElementById('logo').src  = '/static/logo_jvw.svg';
         localStorage.setItem('theme', 'light');
         
     }    
