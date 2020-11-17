a6cced6196a33ca63ca921159c08027fe1992be2
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-11-17 22:29:57 +0100

    added dark mode toggle switch

--- flaskblog/static/script.js
+++ flaskblog/static/script.js
@@ -1,16 +1,4 @@
-// Add reading time to each blog
-var characters = document.getElementById("reading-time").innerHTML;
-var c = parseInt(characters)
-const avgWordsPerMin = 250;
-
-setReadingTime();
-
-function setReadingTime() {
-    let count = c/ 5;
-    let time = Math.ceil(count / avgWordsPerMin);
-    document.getElementById("reading-time").innerHTML = time +  " min. read";
-}
-
+// Add horizontal scroll indicator on blog posts
 window.onscroll = function() {
     myFunction()
 };
@@ -22,3 +10,69 @@ function myFunction() { 
     document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+// Add dark mode
+
+const toggleSwitch = document.querySelector('.nav-link input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        if (document.getElementById('logo').src) {
+            document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
+        } 
+
+        if ( document.getElementById('profile').srcsrc) {
+            document.getElementById('profile').src  = '/static/profile_black.png';
+        } 
+        
+        document.querySelector("link[rel='shortcut icon']").href = "/static/favicon_black.svg";
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        if (document.getElementById('logo').src) {
+            document.getElementById('logo').src  = '/static/logo_jvw_white.svg';
+        } 
+
+        if ( document.getElementById('profile').srcsrc) {
+            document.getElementById('profile').src  = '/static/profile_black.png';
+        } 
+        
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        if (document.getElementById('logo').src) {
+            document.getElementById('logo').src = '/static/logo_jvw.svg';
+        }  
+
+        if (document.getElementById('profile').src) {
+            document.getElementById('profile').src  = '/static/profile.png';
+        } 
+        
+        localStorage.setItem('theme', 'light');
+        
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+// Add reading time to each blog
+var characters = document.getElementById("reading-time").innerHTML;
+var c = parseInt(characters);
+const avgWordsPerMin = 250;
+
+setReadingTime();
+
+function setReadingTime() {
+    let count = c / 5;
+    let time = Math.ceil(count / avgWordsPerMin);
+    document.getElementById("reading-time").innerHTML = time +  " min. read";
+}
